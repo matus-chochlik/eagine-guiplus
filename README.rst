734b9877df5322896bab536e375a56360516b7a8
Matus Chochlik <chochlik@gmail.com>
2023-06-24 15:26:09 +0200

    Multiple changes
    - Created initial directory structure.
    - Added basic build system.
    - Added first example.
    - Added main README file.
    - Added clang-format config.

---
+++ README.rst
@@ -0,0 +1,48 @@
+==============
+EAGine GUIplus
+==============
+
+:Author: Matúš Chochlík <chochlik@gmail.com>
+
+EAGine is a collection of C++ libraries providing set of basic utilities, C++
+wrappers for OpenGL, OpenAL, EGL, SSL, and a message bus useful for writing
+potentially distributed, multi-process applications with graphics rendering
+and/or audio playback.
+
+This repository is one of the sub-modules of the main project hosted
+at https://github.com/matus-chochlik/eagine-all and implements procedural shape
+geometry generators and geometry file loaders.
+
+Cloning the repo
+================
+::
+
+ git clone https://github.com/matus-chochlik/eagine-guiplus.git
+
+Building the code
+=================
+
+The project uses `cmake`-based build system so you can use the following
+to build and install the code:
+
+::
+
+  mkdir -p /path/to/build/dir
+  cd /path/to/build/dir
+  cmake -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=/path/to/install/dir \
+        /path/to/eagine-guiplus/
+  cmake --build . --target install --parallel 16
+
+License
+=======
+
+Copyright Matúš Chochlík, 2015-2023.
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt
+
+The applications using Qt5 are distributed under
+the GNU GENERAL PUBLIC LICENSE version 3.
+See http://www.gnu.org/licenses/gpl-3.0.txt
+
---
+++ example/eagine/guiplus/001_hello.cpp
@@ -0,0 +1,66 @@
+/// @example guiplus/001_hello.cpp
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <GLFW/glfw3.h>
+
+import std;
+import eagine.core;
+import eagine.guiplus;
+
+static void run_loop(GLFWwindow* window, int width, int height) {
+    using namespace eagine::guiplus;
+    (void)window;
+    (void)width;
+    (void)height;
+}
+
+static void init_and_run() {
+    if(not glfwInit()) {
+        throw std::runtime_error("GLFW initialization error");
+    } else {
+        const auto ensure_glfw_cleanup = eagine::finally(glfwTerminate);
+
+        glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+        glfwWindowHint(GLFW_RED_BITS, 8);
+        glfwWindowHint(GLFW_BLUE_BITS, 8);
+        glfwWindowHint(GLFW_GREEN_BITS, 8);
+        glfwWindowHint(GLFW_ALPHA_BITS, 0);
+        glfwWindowHint(GLFW_DEPTH_BITS, 24);
+        glfwWindowHint(GLFW_STENCIL_BITS, 0);
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        int width = 800, height = 600;
+
+        GLFWwindow* window =
+          glfwCreateWindow(width, height, "OGLplus example", nullptr, nullptr);
+
+        if(not window) {
+            throw std::runtime_error("Error creating GLFW window");
+        } else {
+            glfwMakeContextCurrent(window);
+            // const eagine::guiplus::api_initializer gui_api_init;
+            glGetError();
+            run_loop(window, width, height);
+        }
+    }
+}
+
+auto main() -> int {
+    try {
+        init_and_run();
+        return 0;
+    } catch(const std::runtime_error& sre) {
+        std::cerr << "Runtime error: " << sre.what() << std::endl;
+    } catch(const std::exception& se) {
+        std::cerr << "Unknown error: " << se.what() << std::endl;
+    }
+    return 1;
+}
