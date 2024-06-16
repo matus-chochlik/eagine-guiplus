f196c5dd697e687d6269f3dcd5669165fc9fcf17
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:33:52 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageGUIplus.cmake
+++ config/EAGinePackageGUIplus.cmake
@@ -6,7 +6,10 @@
 # Package specific options
 #  Debian
 #   Dependencies
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_GUIPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
 set(CPACK_DEBIAN_GUIPLUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
+set(CPACK_DEBIAN_GUIPLUS-EXAMPLES_DESCRIPTION "EAGine GUIplus examples.")
 set(CPACK_DEBIAN_GUIPLUS-DEV_DESCRIPTION "C++ wrapper for ImGui.")
 
